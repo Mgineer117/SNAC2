a7350a8d6888115ff176d579448235cb6b528885
Minjae Cho <whalswo117@gmail.com>
2024-12-08 13:07:12 -0600

    grad bug fixed

--- models/policy/base_policy.py
+++ models/policy/base_policy.py
@@ -48,39 +48,41 @@ class BasePolicy(nn.Module):
     def compute_gradient_norm(self, models, names, device, dir="None", norm_type=2):
         grad_dict = {}
         for i, model in enumerate(models):
-            total_norm = torch.tensor(0.0, device=device)
-            try:
-                for param in model.parameters():
-                    if (
-                        param.grad is not None
-                    ):  # Only consider parameters that have gradients
-                        param_grad_norm = torch.norm(param.grad, p=norm_type)
-                        total_norm += param_grad_norm**norm_type
-            except:
+            if model is not None:
+                total_norm = torch.tensor(0.0, device=device)
                 try:
-                    param_grad_norm = torch.norm(model.grad, p=norm_type)
+                    for param in model.parameters():
+                        if (
+                            param.grad is not None
+                        ):  # Only consider parameters that have gradients
+                            param_grad_norm = torch.norm(param.grad, p=norm_type)
+                            total_norm += param_grad_norm**norm_type
                 except:
-                    param_grad_norm = torch.tensor(0.0)
-                total_norm += param_grad_norm**norm_type
+                    try:
+                        param_grad_norm = torch.norm(model.grad, p=norm_type)
+                    except:
+                        param_grad_norm = torch.tensor(0.0)
+                    total_norm += param_grad_norm**norm_type
 
-            total_norm = total_norm ** (1.0 / norm_type)
-            grad_dict[dir + "/grad/" + names[i]] = total_norm.item()
+                total_norm = total_norm ** (1.0 / norm_type)
+                grad_dict[dir + "/grad/" + names[i]] = total_norm.item()
 
         return grad_dict
 
     def compute_weight_norm(self, models, names, device, dir="None", norm_type=2):
         norm_dict = {}
         for i, model in enumerate(models):
-            total_norm = torch.tensor(0.0, device=device)
-            try:
-                for param in model.parameters():
-                    param_norm = torch.norm(param, p=norm_type)
+            if model is not None:
+                total_norm = torch.tensor(0.0, device=device)
+                try:
+                    for param in model.parameters():
+                        param_norm = torch.norm(param, p=norm_type)
+                        total_norm += param_norm**norm_type
+                except:
+                    param_norm = torch.norm(model, p=norm_type)
                     total_norm += param_norm**norm_type
-            except:
-                param_norm = torch.norm(model, p=norm_type)
-                total_norm += param_norm**norm_type
-            total_norm = total_norm ** (1.0 / norm_type)
-            norm_dict[dir + "/weight/" + names[i]] = total_norm.item()
+                total_norm = total_norm ** (1.0 / norm_type)
+                norm_dict[dir + "/weight/" + names[i]] = total_norm.item()
 
         return norm_dict
 
